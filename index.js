fac00e967c24a2db74904caed1b981ef2a1e7e3d
jyc-coder <jyc4648@gmail.com>
2022-10-14 23:43:51 +0900

    #1 - API 구현
    데이터 구조
    
        content (메모 내용)
        created_at (생성 시간) 
        updated_at (수정 시간)
        deleted_at (제거 시간)
     
    
    GET
    
    * "/"  - 모든 메모 가져오기 (단, delete_at이 null이 아니면 출력하지 않는다)
    * "/:id" - 단일 메모 가져오기 (만약 delete_at이 null이 아닌 경우 오류메시지 출력)
    * "tmp" - 임시 저장된 데이터 가져오기 (작성하고 생성하지는 않았지만 임시로 저장해놓는 데이터로, 서버를 재시작하면 초기화됨)
    
    POST
    
    * "/" - 메모 생성하기
    
    DELETE 
    
    * "/" - 전체 메모 제거하기 (전체 메모의 `delete_at`에 현재 시간을 추가함)
    * "/:id" - 단일 메모 제거하기 (실질적으로 제거되는 것이 아니라 `delete_at`에 시간을 추가함)
    
    
    PUT
    
    * "/:id" - 단일 메모 수정하기

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express');
+
+let tmp = ""
+const fs = require('fs');
+
+const app = express();
+app.use(express.json());
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+function save() {
+  fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8');
+}
+
+app.get('/', (req, res) => {
+  res.json(data.filter((d) => d.deleted_at === null));
+});
+
+
+
+app.get('/tmp', (req, res) => {
+    res.json({
+      rs: tmp,
+    });
+});
+
+app.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id) || data.length <= id || id < 0) {
+    res.status(400).json({
+      msg: '잘못된 id 값이야',
+    });
+    return;
+  }
+
+  if (data[id].deleted_at !== null) {
+    res.status(404).json({
+      msg: '이미 제거된 메모야',
+    });
+    return;
+  }
+
+  res.json(data[id]);
+});
+
+app.delete('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id) || data.length <= id || id < 0) {
+    res.status(400).json({
+      msg: '잘못된 id 값이야',
+    });
+    return;
+  }
+  if (data[id].deleted_at !== null) {
+    res.status(404).json({
+      msg: '이미 제거된 메모야',
+    });
+    return;
+  }
+
+  data[id].deleted_at = Date.now();
+  res.json(data[id]);
+
+  save();
+});
+
+app.delete('/', (req, res) => {
+    const list = []
+  for (const memo of data) {
+    if (memo.deleted_at === null) {
+        memo.deleted_at = Date.now();
+        list.plush(memo)
+    }
+  }
+  
+  res.json(list);
+
+  save();
+});
+
+app.post('/tmp', (req, res) => {
+    
+    const { content } = req.body;
+
+     if (!content) {
+       res.status(400).json({
+         msg: 'content가 올바르지 않습니다.',
+       });
+       return;
+    }
+    
+    tmp = content;
+    
+    res.json({
+      rs : true
+  });
+});
+
+app.post('/', (req, res) => {
+  const {content} = req.body;
+  if (!content || content.length === 0) {
+    res.status(400).json({
+      msg: 'content가 올바르지 않습니다.',
+    });
+    return;
+  }
+
+  const memo = {
+    content,
+    created_at: Date.now(),
+    updated_at: null,
+    deleted_at: null,
+  };
+
+  data.push(memo);
+
+  res.json(memo);
+
+  save();
+});
+
+app.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const {content} = req.body;
+
+  if (isNaN(id) || data.length <= id || id < 0) {
+    res.status(400).json({
+      msg: '잘못된 id 값이야',
+    });
+    return;
+  }
+  if (!content || content.length === 0) {
+    res.status(400).json({
+      msg: 'content가 올바르지 않습니다.',
+    });
+    return;
+  }
+  if (data[id].deleted_at !== null) {
+    res.status(404).json({
+      msg: '이미 제거된 메모야',
+    });
+    return;
+  }
+  data[id].content = content;
+  data[id].updated_at = Date.now();
+  res.json(data[id]);
+
+  save();
+});
+
+app.listen(8080, () => {
+  console.log('클라우드 메모장 서버 시작');
+});
