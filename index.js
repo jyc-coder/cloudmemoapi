a0aef2fb7acaf3dcf9c9ca21b3f16dbbb0c42bc6
jyc-coder <jyc4648@gmail.com>
2022-10-15 23:47:56 +0900

    #2 - cors 설정 추가
    다른 곳에서 api를 사용할때 오류가 나타나지 않도록하기 위해서 cors 패키지를 설치하고 설정내용을 추가함

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 let tmp = ""
 const fs = require('fs');
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
+app.use(cors)
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 
 function save() {
