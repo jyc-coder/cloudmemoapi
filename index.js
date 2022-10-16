e95f0a3b1ab4e7c3feca72cf996f806b4e0e190d
jyc-coder <jyc4648@gmail.com>
2022-10-16 15:00:42 +0900

    #3- 코드 수정
    cors() 라고 입력했어야하는데 cors 라고만 입력함

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 const app = express();
 app.use(express.json());
-app.use(cors)
+app.use(cors())
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 
 function save() {
